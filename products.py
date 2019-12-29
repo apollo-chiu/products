e3a62b02a0da9c60ee33e25ba3610c99118b171a
Apollo Chiu <eldorado.tw@gmail.com>
2019-12-29 15:43:10 +0800

    append new products

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#先讀取舊檔
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		line = line.encode('utf-8').decode('utf-8-sig')
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+print(products)
+
+
 while 1:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -14,6 +24,7 @@ for d in products:
 	print(d[0], '的價格是', d[1])
 
 with open('products.csv', 'w', encoding='utf-8') as f:
+
 	f.write('商品名稱,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
