8fd3969274a31b54366ea0b49c5a54523467929e
Apollo Chiu <eldorado.tw@gmail.com>
2019-12-29 13:08:22 +0800

    add header

--- products.py
+++ products.py
@@ -13,4 +13,9 @@ while 1:
 for d in products:
 	print(d[0], '的價格是', d[1])
 
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品名稱,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
 		
